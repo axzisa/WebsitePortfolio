aea26a34a6e69169128a3659834e88aabb019433
Надюша <nadezdasestakova902@gmail.com>
2025-03-26 16:47:04 +0500

    comm14.1(photogallery)

---
+++ script.js
@@ -0,0 +1,12 @@
+function openModal(img) {
+    const modal = document.getElementById("modal");
+    const modalImg = document.getElementById("modalImage");
+
+    modal.style.display = "block";
+    modalImg.src = img.src;
+}
+
+function closeModal() {
+    const modal = document.getElementById("modal");
+    modal.style.display = "none";
+}
