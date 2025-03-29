79a8f5ecf5610673ce4f3de35010e8bdf2d0e6fe
Надюша <nadezdasestakova902@gmail.com>
2025-03-29 15:34:09 +0500

    comm18

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
 function openModal(img) {
     const modal = document.getElementById("modal");
     const modalImg = document.getElementById("modalImage");
-
     modal.style.display = "block";
     modalImg.src = img.src;
 }
-
 function closeModal() {
     const modal = document.getElementById("modal");
     modal.style.display = "none";
